9870df8a8638fa3bfef2f57adca127f6464f020c
ParkGana <gnchoco97@gmail.com>
2024-10-17 14:14:49 +0900

    feat : TMDB API에서 받아온 데이터 화면에 그려주기, 포스터 클릭 시 상세 정보 modal 창 띄우기

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,24 @@
 /* DOM 요소 */
-const $movieList = document.querySelectorAll('.movie-poster');
 const $modal = document.querySelector('.modal');
 const $modalClose = document.querySelector('.modal-close');
+const $modalPoster = document.querySelector('.modal-poster > img');
+const $modalTitle = document.querySelector('.modal-title');
+const $modalOverview = document.querySelector('.modal-overview');
+const $modalRelease = document.querySelector('.modal-release');
+const $modalAverage = document.querySelector('.modal-average');
 
 /********************************************************************************/
-
-/* 영화 상세 정보 Modal 창 열기 */
-$movieList.forEach(function (movie) {
-    movie.addEventListener('click', function () {
-        $modal.style.display = 'flex';
-    });
-});
 
 /* 영화 상세 정보 Modal 창 닫기 */
 $modalClose.addEventListener('click', function () {
     $modal.style.display = 'none';
 });
+
+/* 영화 상세 정보 Modal 창에 데이터 넣기 */
+async function printData(data) {
+    $modalPoster.src = `https://image.tmdb.org/t/p/original${data.poster_path}`;
+    $modalTitle.innerText = data.title;
+    $modalOverview.innerText = data.overview;
+    $modalRelease.innerText = data.release_date;
+    $modalAverage.innerText = `⭐ ${data.vote_average}`;
+}
--- js/movie.js
+++ js/movie.js
@@ -1,4 +1,34 @@
-fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc', options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+/* DOM 요소 */
+const $section = document.getElementById('movie-section');
+
+/********************************************************************************/
+
+/* TMDB API를 사용해서 데이터 받아오기 */
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc', options)
+        .then((response) => response.json())
+        .then((response) => {
+            $section.innerHTML = '';
+
+            // 받아온 데이터를 동적으로 화면에 그려주기
+            response.results.map((movie) => {
+                const poster = document.createElement('div');
+                poster.className = 'movie-poster';
+                poster.style.backgroundImage = `url('https://image.tmdb.org/t/p/original${movie.poster_path}')`;
+
+                // 영화 상세 정보 Modal 창 닫기
+                poster.addEventListener('click', async function () {
+                    await printData(movie);
+                    $modal.style.display = 'flex';
+                });
+
+                const title = document.createElement('div');
+                title.className = 'movie-title';
+                title.innerText = movie.title;
+
+                poster.appendChild(title);
+                $section.appendChild(poster);
+            });
+        })
+        .catch((err) => console.error(err));
+});
