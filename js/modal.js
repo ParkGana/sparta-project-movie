d377864268ae8543e08b2b086446e7ea0f3d6e4d
ParkGana <gnchoco97@gmail.com>
2024-10-17 15:51:45 +0900

    feat : 북마크 추가 및 제거하기

--- js/modal.js
+++ js/modal.js
@@ -6,6 +6,7 @@ const $modalTitle = document.querySelector('.modal-title');
 const $modalOverview = document.querySelector('.modal-overview');
 const $modalRelease = document.querySelector('.modal-release');
 const $modalAverage = document.querySelector('.modal-average');
+const $modalBookmark = document.querySelector('.modal-bookmark');
 
 /********************************************************************************/
 
@@ -16,9 +17,36 @@ $modalClose.addEventListener('click', function () {
 
 /* 영화 상세 정보 Modal 창에 데이터 넣기 */
 async function printData(data) {
+    const bookmarkList = JSON.parse(window.localStorage.getItem('bookmarks'));
+
     $modalPoster.src = `https://image.tmdb.org/t/p/original${data.poster_path}`;
     $modalTitle.innerText = data.title;
     $modalOverview.innerText = data.overview;
     $modalRelease.innerText = data.release_date;
     $modalAverage.innerText = `⭐ ${data.vote_average}`;
+    $modalBookmark.id = data.id;
+    $modalBookmark.innerText = `북마크 ${bookmarkList.includes(data.id.toString()) ? '제거' : '추가'}`;
 }
+
+/* 북마크 추가 및 제거하기 */
+$modalBookmark.addEventListener('click', function () {
+    const bookmarkList = JSON.parse(window.localStorage.getItem('bookmarks'));
+
+    // 북마크에 등록되어 있는 경우
+    if (bookmarkList.includes(this.id)) {
+        window.localStorage.setItem('bookmarks', JSON.stringify(bookmarkList.filter((bookmark) => bookmark !== this.id)));
+        this.innerText = '북마크 추가';
+        window.alert('북마크가 제거되었습니다.');
+    }
+    // 북마크에 등록되어있지 않은 경우
+    else {
+        bookmarkList.push(this.id);
+        window.localStorage.setItem('bookmarks', JSON.stringify(bookmarkList));
+        this.innerText = '북마크 제거';
+        window.alert('북마크에 추가되었습니다.');
+    }
+
+    $modal.style.display = 'none';
+
+    window.localStorage.getItem('page-state') === 'total' ? changeToTotal() : changeToBookmark();
+});
--- js/movie.js
+++ js/movie.js
@@ -1,18 +1,32 @@
 /* DOM 요소 */
 const $section = document.getElementById('movie-section');
+const $totalBtn = document.querySelector('.logo');
+const $bookmarkBtn = document.querySelector('.bookmark');
+
+let dataList;
 
 /********************************************************************************/
 
 /* TMDB API를 사용해서 데이터 받아오기 */
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
+    window.localStorage.setItem('page-state', 'total');
+
+    // localStorage에 bookmarks 데이터 생성하기 (없을 경우)
+    if (!window.localStorage.getItem('bookmarks')) {
+        window.localStorage.setItem('bookmarks', JSON.stringify([]));
+    }
+
     fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc', options)
         .then((response) => response.json())
         .then((response) => {
+            dataList = response.results;
+
             $section.innerHTML = '';
 
             // 받아온 데이터를 동적으로 화면에 그려주기
             response.results.map((movie) => {
                 const poster = document.createElement('div');
+                poster.id = movie.id;
                 poster.className = 'movie-poster';
                 poster.style.backgroundImage = `url('https://image.tmdb.org/t/p/original${movie.poster_path}')`;
 
@@ -32,3 +46,35 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch((err) => console.error(err));
 });
+
+/* 로고 버튼 클릭 이벤트 */
+$totalBtn.addEventListener('click', changeToTotal);
+
+/* 북마크 버튼 클릭 이벤트 */
+$bookmarkBtn.addEventListener('click', changeToBookmark);
+
+/********************************************************************************/
+
+/* 페이지를 전체 모드로 변환하기 */
+function changeToTotal() {
+    const $movieList = document.querySelectorAll('.movie-poster');
+
+    window.localStorage.setItem('page-state', 'total');
+
+    $movieList.forEach((movie) => {
+        movie.style.display = 'flex';
+    });
+}
+
+/* 페이지를 북마크 모드로 변환하기 */
+function changeToBookmark() {
+    const $movieList = document.querySelectorAll('.movie-poster');
+
+    window.localStorage.setItem('page-state', 'bookmark');
+
+    const bookmarkList = JSON.parse(window.localStorage.getItem('bookmarks'));
+
+    $movieList.forEach((movie) => {
+        movie.style.display = bookmarkList.includes(movie.id) ? 'flex' : 'none';
+    });
+}
